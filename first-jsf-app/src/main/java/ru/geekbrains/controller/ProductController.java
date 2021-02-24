c811ddf384452b60e345c1b3474c1d5e1d7dbbce
egorbarinov <egor.barinov@gmail.com>
2021-02-24 10:56:57 +0300

    добавлен код с урока4

---
+++ first-jsf-app/src/main/java/ru/geekbrains/controller/ProductController.java
@@ -0,0 +1,54 @@
+package ru.geekbrains.controller;
+
+import ru.geekbrains.persist.Product;
+import ru.geekbrains.persist.ProductRepository;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.List;
+
+@Named
+@SessionScoped
+public class ProductController implements Serializable {
+
+    @Inject
+    private ProductRepository productRepository;
+
+    private Product product;
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public String createProduct() {
+        this.product = new Product();
+        return "/product_form.xhtml?faces-redirect-true";
+    }
+
+    // http://127.0.0.1:8080/first-jsf-app//product.xhtml
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public String editProduct(Product product) {
+        this.product = product;
+        return "/product_form.xhtml?faces-redirect-true";
+    }
+
+    public void deleteProduct(Product product) {
+        productRepository.deleteById(product.getId());
+    }
+
+
+    //http://127.0.0.1:8080/first-jsf-app//product.xhtml
+    public String saveProduct() {
+        productRepository.saveOrUpdate(product);
+        return "/product.xhtml?faces-redirect-true";
+    }
+}
