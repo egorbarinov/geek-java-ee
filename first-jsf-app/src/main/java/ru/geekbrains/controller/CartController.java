e2330b3c654bed59853af7db6b659e088ab590c3
egorbarinov <59361760+egorbarinov@users.noreply.github.com>
2021-03-03 19:54:06 +0300

    Lesson05 (#6)
    * добавлен код с урока 4
    
    * added code from the homework assign from the lesson04
    
    * добавил код с урока

--- first-jsf-app/src/main/java/ru/geekbrains/controller/CartController.java
+++ first-jsf-app/src/main/java/ru/geekbrains/controller/CartController.java
@@ -5,13 +5,10 @@ import ru.geekbrains.persist.Product;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 import java.io.Serializable;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
-
 
 @Named
 @SessionScoped
